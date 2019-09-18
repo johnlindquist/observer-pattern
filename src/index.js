3ba54b15deaf5cb411738cb43e76e72c548f4498
John Lindquist <johnlindquist@gmail.com>
2019-09-18 11:36:18 -0600

    Wed Sep 18 11:36:18 MDT 2019

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ const observer = {
   }
 }
 
-const createObservable = observer => {
-  observer.notify({ message: "hello" })
+const createObservable = subscribe => {
+  return {
+    subscribe
+  }
 }
 
-const observable = createObservable(observer)
+const subscribe = observer => {
+  document.addEventListener("click", observer.notify)
+
+  const unsubscribe = () => {
+    document.removeEventListener("click", observer.notify)
+  }
+
+  return unsubscribe
+}
+
+const observable = createObservable(subscribe)
+
+const unsubscribe = observable.subscribe(observer)
+//unsubscribe
