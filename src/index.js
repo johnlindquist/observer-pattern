2bab7f5cd3a6d9b11972976b96f87165e83591a7
John Lindquist <johnlindquist@gmail.com>
2019-09-18 10:56:55 -0600

    Wed Sep 18 10:56:55 MDT 2019

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
-const subject = {
-  state: {
-    message: "Hello"
-  },
-  observers: [],
-  update(value) {
-    this.state = value
-    this.observers.forEach(observer => {
-      observer.notify()
-    })
+const observer = {
+  notify(value) {
+    //notify is named `next` in RxJS
+    console.log(`new value received`, value)
   }
 }
 
-const createObserver = subject => {
-  const observer = {
-    subject,
-    notify() {
-      console.log("State updated to", this.subject.state)
-    }
-  }
-
-  subject.observers.push(observer)
-  return observer
+const createObservable = observer => {
+  observer.notify({ message: "hello" })
 }
 
-const observer1 = createObserver(subject)
-const observer2 = createObserver(subject)
-
-subject.update({ message: "Goodbye" })
+const observable = createObservable(observer)
