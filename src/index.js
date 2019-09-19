582868d77e995c6447cb44dab2e1aeb3dfc5e42b
John Lindquist <johnlindquist@gmail.com>
2019-09-19 06:01:04 -0600

    Thu Sep 19 06:01:04 MDT 2019

--- src/index.js
+++ src/index.js
@@ -4,32 +4,21 @@ const observer = {
   }
 }
 
-const createSubject = subscribe => {
+const createSubject = () => {
   let intervalObserver
-  const internalSubscribe = observer => {
-    intervalObserver = observer
-    subscribe(observer)
-  }
 
   return {
-    subscribe: internalSubscribe,
+    subscribe: observer => {
+      intervalObserver = observer
+    },
     next: value => {
       intervalObserver.next(value)
     }
   }
 }
 
-const subscribe = observer => {
-  document.addEventListener("click", observer.next)
-
-  const unsubscribe = () => {
-    document.removeEventListener("click", observer.next)
-  }
-
-  return unsubscribe
-}
-
-const subject = createSubject(subscribe)
+const subject = createSubject()
 subject.subscribe(observer)
 
 subject.next({ message: "Hello" })
+subject.next({ message: "Goodbye" })
