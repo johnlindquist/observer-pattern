16ca8db24f6f3c4fcfcd6fe7d3a4082461fd4b6b
John Lindquist <johnlindquist@gmail.com>
2019-09-19 17:25:50 -0600

    Thu Sep 19 17:25:50 MDT 2019

--- src/index.js
+++ src/index.js
@@ -1,56 +1,11 @@
-const oneObserver = {
-  next(value) {
-    console.log(`one`, value)
+const next = value => {
+  console.log(value)
+}
+
+const observable = {
+  subscribe: next => {
+    next("hello")
   }
 }
 
-const twoObserver = {
-  next(value) {
-    console.log(`two`, value)
-  }
-}
-
-const createObservable = subscribe => {
-  return {
-    subscribe
-  }
-}
-
-const subscribe = observer => {
-  document.addEventListener("click", observer.next)
-
-  const unsubscribe = () => {
-    document.removeEventListener("click", observer.next)
-  }
-
-  return unsubscribe
-}
-
-const createSubject = () => {
-  let observers = []
-
-  return {
-    subscribe: observer => {
-      observers.push(observer)
-
-      const unsubscribe = () => {
-        const index = observers.indexOf(observer)
-        observers.splice(index, 1)
-      }
-
-      return unsubscribe
-    },
-    next: value => {
-      observers.forEach(observer => {
-        observer.next(value)
-      })
-    }
-  }
-}
-
-const subject = createSubject()
-const observable = createObservable(subscribe)
-
-subject.subscribe(oneObserver)
-subject.subscribe(twoObserver)
-observable.subscribe(subject)
+observable.subscribe(next)
