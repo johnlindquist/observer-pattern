282c2075d8d1f7c678c5abb13f07a4073811f628
John Lindquist <johnlindquist@gmail.com>
2019-09-20 09:29:37 -0600

    Fri Sep 20 09:29:37 MDT 2019

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import { fromEvent } from "rxjs"
 
 const operator = observable => {
-  observable.subscribe(value => {
-    console.log("hi")
-  })
+  const newObservable = {
+    //1. Create a new Observable
+    subscribe: next => {
+      observable.subscribe(value => {
+        //2. Subscribe to the original
+        next("hi") //3. Pass a different value to `next`
+      })
+    }
+  }
 
-  return observable
+  return newObservable
 }
 
 const click = fromEvent(document, "click").pipe(operator)
