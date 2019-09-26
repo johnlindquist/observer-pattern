46d1065059686383c5d390f655608ffe4c31581a
John Lindquist <johnlindquist@gmail.com>
2019-09-26 10:09:33 -0600

    Thu Sep 26 10:09:33 MDT 2019

--- src/index.js
+++ src/index.js
@@ -1,192 +1,21 @@
-import { Observable } from "rxjs"
-
-//## Creation Operators
-
-const creationOperatorTemplate = config => {
-  const source = new Observable(observer => {
-    //Use observer to next, complete, error
-    return () => {
-      //unsubscribe logic
-    }
-  })
-
-  return source
+const source1 = callback => {
+  document.addEventListener("click", callback)
 }
 
-//of
-const of = value => {
-  const source = new Observable(observer => {
-    observer.next(value)
-    observer.complete()
-  })
-
-  return source
+const source2 = callback => {
+  setTimeout(callback, 1000)
 }
 
-of("hello").subscribe(console.log)
-
-//fromEvent
-const fromEvent = (target, eventType) => {
-  const source = new Observable(observer => {
-    const next = observer.next.bind(observer)
-    target.addEventListener(eventType, next)
-
-    return () => {
-      target.removeEventListener(eventType, next)
-    }
+const mergeMap = customize => source1 => callback =>
+  source1(event => {
+    const custom = customize(event)
+    custom(callback)
   })
 
-  return source
-}
+const mergeSource2 = mergeMap(event => source2)
 
-//interval
-const interval = delay => {
-  const source = new Observable(observer => {
-    const next = observer.next.bind(observer)
+const newSource = mergeSource2(source1)
 
-    let i = 0
-    const callback = () => {
-      next(i++)
-    }
-    const id = setInterval(callback, delay)
-
-    return () => {
-      console.log("clear")
-      clearInterval(id)
-    }
-  })
-
-  return source
-}
-
-const click = fromEvent(document, "click")
-
-//merge
-const merge = (...observables) => {
-  const source = new Observable(observer => {
-    const next = observer.next.bind(observer)
-    const complete = observer.complete.bind(observer)
-    const error = observer.error.bind(observer)
-
-    let active = observables.length
-
-    const subscriptions = observables.map(observable => {
-      const newObserver = {
-        next,
-        error,
-        complete: () => {
-          active--
-          if (active === 0) {
-            complete()
-          }
-        }
-      }
-
-      const subscription = observable.subscribe(newObserver)
-
-      return subscription
-    })
-
-    return () => {
-      subscriptions.forEach(subscription => {
-        subscription.unsubscribe()
-      })
-    }
-  })
-
-  return source
-}
-
-// merge(of("merge"), interval(1000)).subscribe(console.log)
-
-//copy/paste this as a starter for new operators
-const operatorTemplate = config => source => {
-  const newSource = new Observable(observer => {
-    //Use `config` here to do whatever you need
-    const newObserver = {
-      next: value => {
-        observer.next(value)
-      }
-    }
-    source.subscribe(newObserver)
-  })
-
-  return newSource
-}
-
-//mapTo
-const mapTo = value => source => {
-  const newSource = new Observable(observer => {
-    const newObserver = {
-      next: () => {
-        observer.next(value)
-      }
-    }
-    source.subscribe(newObserver)
-  })
-
-  return newSource
-}
-
-// click.pipe(mapTo("hello")).subscribe(console.log)
-
-//map
-const map = fn => source => {
-  const newSource = new Observable(observer => {
-    const newOperator = {
-      next: value => {
-        const mappedValue = fn(value)
-        observer.next(mappedValue)
-      }
-    }
-    source.subscribe(newOperator)
-  })
-
-  return newSource
-}
-
-// click.pipe(map(event => event.target)).subscribe(console.log)
-
-//filter
-const filter = fn => source => {
-  const newSource = new Observable(observer => {
-    const newOperator = {
-      next: value => {
-        if (fn(value)) {
-          observer.next(value)
-        }
-      }
-    }
-    source.subscribe(newOperator)
-  })
-
-  return newSource
-}
-
-// click.pipe(filter(event => event.x > 200)).subscribe(console.log)
-
-const combineLatest = (...observables) => {
-  return new Observable(observer => {
-    const waiting = Symbol("waiting")
-    const values = Array.from({ length: observables.length }).fill(waiting)
-
-    const subscriptions = observables.map((observable, i) => {
-      const subscription = observable.subscribe(value => {
-        values[i] = value
-        if (values.every(value => value !== waiting)) {
-          observer.next(values)
-        }
-      })
-
-      return subscription
-    })
-
-    return () => {
-      subscriptions.forEach(subscription => {
-        subscription.unsubscribe()
-      })
-    }
-  })
-}
-
-combineLatest(click, interval(500)).subscribe(console.log)
+newSource(() => {
+  console.log("timeout")
+})
